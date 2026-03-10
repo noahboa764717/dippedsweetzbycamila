e596794e81c357efc0f094e0f6be434dac5f2c00
noahboa764717 <108681262+noahboa764717@users.noreply.github.com>
2026-03-09 20:44:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
-const observer = new IntersectionObserver(entries => {
+const galleryFolder = "gallery/"
 
-entries.forEach(entry => {
+const imageFiles = [
+"IMG_1888.jpeg",
+"IMG_1889.jpeg",
+"bouquet1.png",
+"box1.png",
+"dessertbox1.png",
+"strawberry1.png",
+"strawberry2.png",
+"strawberry3.png"
+]
 
-if(entry.isIntersecting){
+const gallery = document.getElementById("gallery")
 
-entry.target.classList.add("show")
+imageFiles.forEach((file,index)=>{
+
+const img = document.createElement("img")
+
+img.src = galleryFolder + file
+
+img.onclick = () => openLightbox(index)
+
+gallery.appendChild(img)
+
+})
+
+
+let currentIndex = 0
+
+function openLightbox(index){
+
+currentIndex = index
+
+document.getElementById("lightbox").style.display="flex"
+
+document.getElementById("lightbox-img").src = galleryFolder + imageFiles[index]
 
 }
 
-})
+function closeLightbox(){
 
-})
+document.getElementById("lightbox").style.display="none"
 
-document.querySelectorAll(".fade").forEach(el => {
-
-observer.observe(el)
-
-})
+}
